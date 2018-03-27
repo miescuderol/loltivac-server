6ce4cc30b3614514beae3b567e4405ca403891f9
Nacha Escudero Leoz <miescuderol@gmail.com>
2018-03-26 20:37:31 -0700

    changed to ts. call to match endpoint

--- app.ts
+++ app.ts
@@ -3,10 +3,14 @@ const https = require('https');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+const summonerEndpoint = '/lol/summoner/v3/summoners/by-name/';
+const matchesByAccountId = '/lol/match/v3/matchlists/by-account/';
+const matchById = '/lol/match/v3/matches/'
 
 const server = http.createServer((req, res) => {
     const { headers, method, url } = req;
     let body = [];
+    let partialData;
     req.on('error', (err) => {
         console.error(err);
         res.statusCode = 400;
@@ -16,28 +20,37 @@ const server = http.createServer((req, res) => {
         console.error(err);
     });
 
-    getsummonerInfo('dooky18', (statusCode, data) => {
-        console.log("onResult: (" + statusCode + ")" + JSON.stringify(data));
+
+    callEndpoint(summonerEndpoint + 'dooky18', (statusCode, data) => {
         res.statusCode = statusCode;
+        partialData = JSON.parse(data);
+        console.log(partialData.accountId);
+        callEndpoint(matchesByAccountId + partialData.accountId, (statusCode, data) => {
+            res.statusCode = statusCode;
+            console.log(matchesByAccountId, 'response:', data);
+            res.end(data);
+        });
         res.end(data);
     });
 
 });
+
 // TODO: hacer algo
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-function getsummonerInfo(summonerName, callback) {
-    console.log('getsummonername')
+function callEndpoint(endpoint, callback) {
+    console.log('getting:', endpoint);
     const headers = {
-        "X-Riot-Token": "RGAPI-fcb90cbb-94da-425d-be0b-7cafb6a3ab53"}
+        "X-Riot-Token": "RGAPI-be5d84d6-0b72-470d-902b-cc51f7197d37"
+    };
     const options = {
         host: 'na1.api.riotgames.com',
-        path: '/lol/summoner/v3/summoners/by-name/dooky18',
+        path: endpoint,
         method: 'GET',
         headers: headers
-    }
+    };
     let req = https.request(options, (response) => {
         console.log('request');
         console.log(response.statusCode);
@@ -47,7 +60,6 @@ function getsummonerInfo(summonerName, callback) {
         });
 
         response.on('end', () => {
-            data = JSON.stringify(data);
             callback(response.statusCode, data);
         })
     });
